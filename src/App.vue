54ba3ac14f4a44d9a6b4acee91bffb7a89235d73
Josh Justice <jjustice@bignerdranch.com>
2017-12-31 09:02:41 -0500

    Create new message form component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
+    <new-message-form />
   </div>
 </template>
 
 <script>
+import NewMessageForm from './components/NewMessageForm';
+
 export default {
   name: 'app',
+  components: {
+    NewMessageForm,
+  },
 };
 </script>
 
---
+++ src/components/NewMessageForm.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'new-message-form',
+};
+</script>
