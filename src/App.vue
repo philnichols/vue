4f38bf4de3786874f73c5bdab6c4c1217c56f5d2
Josh Justice <me@codingitwrong.com>
2019-02-06 07:02:19 -0500

    Save messages in list

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <new-message-form />
+    <new-message-form @send="addMessage" />
   </div>
 </template>
 
@@ -11,6 +11,16 @@ export default {
   name: 'App',
   components: {
     NewMessageForm,
+  },
+  data() {
+    return {
+      messages: [],
+    };
+  },
+  methods: {
+    addMessage(text) {
+      this.messages.unshift(text);
+    },
   },
 };
 </script>
