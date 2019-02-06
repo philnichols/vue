23f02a6505eb38e15cb115700266a79137abd8f1
Josh Justice <me@codingitwrong.com>
2019-02-06 07:02:51 -0500

    Add empty MessageList component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <new-message-form @send="addMessage" />
+    <message-list :messages="messages" />
   </div>
 </template>
 
 <script>
 import NewMessageForm from './components/NewMessageForm';
+import MessageList from './components/MessageList';
 
 export default {
   name: 'App',
   components: {
     NewMessageForm,
+    MessageList,
   },
   data() {
     return {
