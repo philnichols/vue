b9a231a0982eb12283486f88b440db93b0d9446a
Josh Justice <me@codingitwrong.com>
2019-02-06 06:37:13 -0500

    Add empty NewMessageForm component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
+    <new-message-form />
   </div>
 </template>
 
 <script>
+import NewMessageForm from './components/NewMessageForm';
+
 export default {
   name: 'App',
+  components: {
+    NewMessageForm,
+  },
 };
 </script>
